756cffce3a1e00ba6e88c82d2731e509d2019063
“Johnny <jaaragon@pucesi.edu.ec>
2025-05-29 19:03:29 -0500

    1

---
+++ Ejercicio4/Funciones/Scipt.js
@@ -0,0 +1,26 @@
+function AgregarTarea() {
+    // Exraccion de objetos
+    const input = document.getElementById("inpTexto");
+    const texto = input.value;
+    const lista = document.getElementById("ulLista");
+    // Validacion de que no ingrese texto vacio
+    if (texto === "") {
+        alert("No hay texto");
+    }
+    // Crear elementos desde JS que son html
+    const elementoLi = document.createElement("li");
+    const contenidoTexto = document.createElement("span");
+    contenidoTexto.textContent = texto;
+    // Solo del boton
+    const btnEliminar = document.createElement("button");
+    btnEliminar.textContent = "Eliminar Actividad";
+    btnEliminar.onclick = () => {
+        lista.removeChild(elementoLi);
+    };
+    // Agregar elemento y contenido a la lista y li
+    elementoLi.appendChild(contenidoTexto);
+    elementoLi.appendChild(btnEliminar);
+    lista.appendChild(elementoLi);
+    // Blanquear input
+    input.value = "";
+}
